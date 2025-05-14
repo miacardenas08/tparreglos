2f43eae9540180b500d8d0af8704cb87642de09f
alumno <alumno@Instituto.local>
2025-05-14 08:16:51 -0300

    cambios

---
+++ arreglos.js
@@ -0,0 +1,11 @@
+let colores = ["rojo", "verde","rosa", "azul", "violeta"];
+console.log(colores.length);
+
+let primero = colores[0];
+let ultimo = colores[colores.length-1];
+console.log(primero);
+console.log(ultimo);
+
+
+colores.push("naranja");
+console.log()

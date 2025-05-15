78d0d613cf3a6415ba5481483154069dba8b669e
alumno <alumno@Instituto.local>
2025-05-15 11:13:30 -0300

    Update arreglos.js

--- arreglos.js
+++ arreglos.js
@@ -1,11 +1,32 @@
+//1
 let colores = ["rojo", "verde","rosa", "azul", "violeta"];
-console.log(colores.length);
 
+//2
 let primero = colores[0];
 let ultimo = colores[colores.length-1];
 console.log(primero);
 console.log(ultimo);
 
+//3
+console.log("el tamaño del arreglo es " + colores.length);
 
+//4
 colores.push("naranja");
 console.log(colores[colores.length-1]);
+
+//5
+colores.unshift("blanco");
+console.log(colores[0]);
+
+//6 
+colores.pop();
+console.log(colores);
+
+//7
+colores.shift();
+console.log(colores);
+
+//8
+colores[1] = "marron";
+console.log(colores);
+
